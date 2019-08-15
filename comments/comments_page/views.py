ca7f93c3d5b1d3b0d8f2447e8b6f3c6e1e234c98
Natalia <n.platova@hotmail.com>
2019-08-15 19:42:11 +0700

    html page; fix #3

--- comments/comments_page/views.py
+++ comments/comments_page/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Review
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    review_list = Review.objects.order_by('-pub_date')
+    context = {
+        'review_list': review_list,
+    }
+
+    return render(request, 'comments_page/index.html', context)
